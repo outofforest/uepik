c990c91db243d9dc99f10c10d01d8ed71531514e
Wojciech Malota-Wojcik <wojtek@exw.co>
2025-09-20 18:59:27 +0200

    Financial flow report

--- generators/formatting.go
+++ generators/formatting.go
@@ -2,6 +2,7 @@ package generators
 
 import (
 	"strings"
+	"time"
 
 	"github.com/samber/lo"
 	"github.com/xuri/excelize/v2"
@@ -37,5 +38,36 @@ func excelNumberFormat(precision uint64) *excelize.Style {
 	}
 	return &excelize.Style{
 		CustomNumFmt: lo.ToPtr(format),
+	}
+}
+
+func monthName(month time.Month) string {
+	switch month {
+	case time.January:
+		return "styczeń"
+	case time.February:
+		return "luty"
+	case time.March:
+		return "marzec"
+	case time.April:
+		return "kwiecień"
+	case time.May:
+		return "maj"
+	case time.June:
+		return "czerwiec"
+	case time.July:
+		return "lipiec"
+	case time.August:
+		return "sierpień"
+	case time.September:
+		return "wrzesień"
+	case time.October:
+		return "październik"
+	case time.November:
+		return "listopad"
+	case time.December:
+		return "grudzień"
+	default:
+		panic("invalid month")
 	}
 }
