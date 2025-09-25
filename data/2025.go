b897e4bd21e601760bed8690aded0cee45ddb87e
Wojciech Malota-Wojcik <wojtek@exw.co>
2025-09-25 20:52:07 +0200

    Split records between months

--- data/2025.go
+++ data/2025.go
@@ -6,9 +6,9 @@ import . "github.com/outofforest/uepik" //nolint:staticcheck
 // R2025 to dane księgowe za rok 2025.
 var R2025 = Rok(2025, kursy2025,
 	Darowizna(
-		Dokument("WB/EUR/2025/01/01", Data(2025, 1, 3)),
+		Dokument("WB/EUR/2025/01/01", Data(2025, 5, 3)),
 		Kontrahent("INVINI sp. z o. o.", "Felińskiego 2/17", ""),
-		Platnosc(Kwota(500, 0, EUR), Data(2025, 1, 3), 1)),
+		Platnosc(Kwota(500, 0, EUR), Data(2025, 5, 3), 1)),
 	rejs2026HR01,
 )
 
@@ -17,4 +17,5 @@ var kursy2025 = Kursy(
 	Kurs(EUR, Data(2025, 1, 1), 4, 5200),
 	Kurs(EUR, Data(2025, 1, 2), 4, 4300),
 	Kurs(EUR, Data(2025, 1, 7), 4, 4800),
+	Kurs(EUR, Data(2025, 5, 2), 4, 4300),
 )
